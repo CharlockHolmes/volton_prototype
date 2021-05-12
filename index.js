58e2c34bbec62ca66a4e541a5fa9572d3bee6510
CharlockHolmes <70542173+CharlockHolmes@users.noreply.github.com>
2021-05-12 15:36:43 -0400

    Initialization

---
+++ index.js
@@ -0,0 +1,34 @@
+
+
+const express = require('express');
+const Datastore = require('nedb'); 
+const app = express();
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Starting server at ${port}`));
+app.use(express.static('public'));
+app.use(express.json({
+    limit: '10mb'
+}));
+
+const database = new Datastore('database.db');
+// database.loadDatabase();
+
+// app.post('/api', (request, response) => {
+//     console.log("i got a request");
+//     const data = request.body;
+//     database.insert({data});
+//     console.log(data);
+//     response.json(data);
+//     //response.end(); //bare minimum require to make it work, needs to send something back
+// });
+
+// app.get('/api', (request, response)=>{
+//     database.find({}, (err, data) => {
+//         if(err){
+//             response.end();
+//             return;
+//         }
+//         response.json(data);
+//     });
+    
+// });
