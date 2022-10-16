77b8b87fb63ffee2dc3edaf8cbc7e95dd16cecd4
CharlockHolmes <70542173+CharlockHolmes@users.noreply.github.com>
2022-10-16 19:59:49 -0400

    Complete test

--- public/NewMicaBand/builder.js
+++ public/NewMicaBand/builder.js
@@ -1,28 +1,29 @@
-class Builder{
-    constructor(){
-        if(CAPTCHA_Validate()){
-
-            createRenderer();
-            createScene();
-            loadSavedValues();
-            letInit();
-            loadTextField()
-            this.tutorial
-            this.gapTable
-            this.layout;
-            this.componentTable;
-            setTimeout(()=>{
-                this.tutorial = new TutorialHandler();
-                this.gapTable = new DomTable(r.gaps.length, r.radius, inchPerUnit);
-                this.gapTable.uploadTableData();
-                this.layout = new LayoutHandler()
-                this.componentTable = new ComponentTableManager();
-                document.getElementById('loadingbanner').innerHTML = '';
-            },1000);
-        }
-
+class Builder {
+  constructor() {
+    if (CAPTCHA_Validate()) {
+      console.log("ayyaya");
+      createRenderer();
+      createScene();
+      loadSavedValues();
+      letInit();
+      loadTextField();
+      this.tutorial;
+      this.gapTable;
+      this.layout;
+      this.componentTable;
+      setTimeout(() => {
+        this.tutorial = new TutorialHandler();
+        this.gapTable = new DomTable(r.gaps.length, r.radius, inchPerUnit);
+        this.gapTable.uploadTableData();
+        this.layout = new LayoutHandler();
+        this.componentTable = new ComponentTableManager();
+        document.getElementById("loadingbanner").innerHTML = "";
+      }, 1000);
     }
+  }
 }
 
-const build = new Builder()
-document.getElementById('retro').onclick = () =>{build.layout.specialMode();}
+const build = new Builder();
+document.getElementById("retro").onclick = () => {
+  build.layout.specialMode();
+};
