0af514cdd7f35b80a5d79c6132e2a8905f7147b6
CharlockHolmes <70542173+CharlockHolmes@users.noreply.github.com>
2023-09-14 10:35:42 -0400

    test

--- index.js
+++ index.js
@@ -136,6 +136,7 @@ app.post("/api", (request, response) => {
 });
 
 function sendMail(text = "nothing was inserted", clientmail, mailinfo) {
+  console.log("Email sending with mailtrap");
   var mailOptions = {
     from: "no-reply@voltondesign.com",
     //to: ["info@volton.com", clientmail],
@@ -151,16 +152,6 @@ function sendMail(text = "nothing was inserted", clientmail, mailinfo) {
       content: text,
     },
   };
-  // var mailOptions2 = {
-  //     from: 'testvolton2021@gmail.com',
-  //     to: clientmail,
-  //     subject: 'Submission copy - Volton',
-  //     html: '<p>'+'You may find a copy of your submission in the link.html file attached to this document.<br> A representative will contact you to confirm the order.'+'</p>',
-  //     attachments: {   // utf-8 string as an attachment
-  //         filename: 'link.html',
-  //         content: text
-  //     }
-  // };
   console.log("something is happening");
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
@@ -171,95 +162,7 @@ function sendMail(text = "nothing was inserted", clientmail, mailinfo) {
   });
 }
 
-// const fs = require('fs').promises;
-// const path = require('path');
-// const process = require('process');
-// const {authenticate} = require('@google-cloud/local-auth');
-// const {google} = require('googleapis');
 
-// // If modifying these scopes, delete token.json.
-// const SCOPES = ['https://mail.google.com/'];
-// // The file token.json stores the user's access and refresh tokens, and is
-// // created automatically when the authorization flow completes for the first
-// // time.
-// const TOKEN_PATH = path.join(process.cwd(), 'token.json');
-// const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
-
-// /**
-//  * Reads previously authorized credentials from the save file.
-//  *
-//  * @return {Promise<OAuth2Client|null>}
-//  */
-// async function loadSavedCredentialsIfExist() {
-//   try {
-//     const content = await fs.readFile(TOKEN_PATH);
-//     const credentials = JSON.parse(content);
-//     return google.auth.fromJSON(credentials);
-//   } catch (err) {
-//     return null;
-//   }
-// }
-
-// /**
-//  * Serializes credentials to a file comptible with GoogleAUth.fromJSON.
-//  *
-//  * @param {OAuth2Client} client
-//  * @return {Promise<void>}
-//  */
-// async function saveCredentials(client) {
-//   const content = await fs.readFile(CREDENTIALS_PATH);
-//   const keys = JSON.parse(content);
-//   const key = keys.installed || keys.web;
-//   const payload = JSON.stringify({
-//     type: 'authorized_user',
-//     client_id: key.client_id,
-//     client_secret: key.client_secret,
-//     refresh_token: client.credentials.refresh_token,
-//   });
-//   await fs.writeFile(TOKEN_PATH, payload);
-// }
-
-// /**
-//  * Load or request or authorization to call APIs.
-//  *
-//  */
-// async function authorize() {
-//   let client = await loadSavedCredentialsIfExist();
-//   if (client) {
-//     return client;
-//   }
-//   client = await authenticate({
-//     scopes: SCOPES,
-//     keyfilePath: CREDENTIALS_PATH,
-//   });
-//   if (client.credentials) {
-//     await saveCredentials(client);
-//   }
-//   return client;
-// }
-
-// /**
-//  * Lists the labels in the user's account.
-//  *
-//  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
-//  */
-// async function listLabels(auth) {
-//   const gmail = google.gmail({version: 'v1', auth});
-//   const res = await gmail.users.labels.list({
-//     userId: 'me',
-//   });
-//   const labels = res.data.labels;
-//   if (!labels || labels.length === 0) {
-//     console.log('No labels found.');
-//     return;
-//   }
-//   console.log('Labels:');
-//   labels.forEach((label) => {
-//     console.log(`- ${label.name}`);
-//   });
-// }
-
-// authorize().then(listLabels).catch(console.error);
 // function testmail() {
 //   var transport = nodemailer.createTransport({
 //     host: "live.smtp.mailtrap.io",
